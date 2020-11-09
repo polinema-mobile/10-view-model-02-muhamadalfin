7c788efcaefcac5b993efd5d788489d5988920f0
muhamadalfin <malfin416@gmail.com>
2020-11-09 10:15:32 +0700

    Update Home Login Splash

--- app/src/main/java/id/putraprima/mvvmlogin/fragments/HomeFragment.java
+++ app/src/main/java/id/putraprima/mvvmlogin/fragments/HomeFragment.java
@@ -1,7 +1,10 @@
 package id.putraprima.mvvmlogin.fragments;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,8 +12,13 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import id.putraprima.mvvmlogin.R;
+import id.putraprima.mvvmlogin.databinding.FragmentHomeBinding;
+import id.putraprima.mvvmlogin.databinding.FragmentLoginBinding;
+import id.putraprima.mvvmlogin.models.User;
 
 public class HomeFragment extends Fragment {
+
+    private FragmentHomeBinding fragmentHomeBinding;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -20,6 +28,14 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        fragmentHomeBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false);
+        return fragmentHomeBinding.getRoot();
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        assert getArguments() != null;
+        //User user  =  HomeFragmentArgs
     }
 }
--- app/src/main/java/id/putraprima/mvvmlogin/fragments/LoginFragment.java
+++ app/src/main/java/id/putraprima/mvvmlogin/fragments/LoginFragment.java
@@ -2,15 +2,29 @@ package id.putraprima.mvvmlogin.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import id.putraprima.mvvmlogin.R;
+import id.putraprima.mvvmlogin.databinding.FragmentLoginBinding;
+import id.putraprima.mvvmlogin.models.User;
+import id.putraprima.mvvmlogin.viewModel.LoginViewModel;
 
 public class LoginFragment extends Fragment {
+
+    private FragmentLoginBinding fragmentLoginBinding;
+    private LoginViewModel loginViewModel;
+
     public LoginFragment() {
         // Required empty public constructor
     }
@@ -20,6 +34,13 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        fragmentLoginBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_login,container, false);
+        loginViewModel = new ViewModelProvider(this).get(LoginViewModel.class);
+        fragmentLoginBinding.setViewModel(loginViewModel);
+        fragmentLoginBinding.setLifecycleOwner(this);
+        return fragmentLoginBinding.getRoot();
     }
+
+
+
 }
